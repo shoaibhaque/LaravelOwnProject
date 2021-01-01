d94399551f6964f5c77650e3447967cb2c02ec9f
shoaibhaque <showaibulchowdhury>
2021-01-01 18:04:59 +0600

    uploading all files

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<p>Welcome To Home From Shoaib Haque</p>
+	<p>Nice Work! From Shoaib2018</p>
+	<p>Thanks from ShoaibHaque!</p>
+</body>
+</html>
